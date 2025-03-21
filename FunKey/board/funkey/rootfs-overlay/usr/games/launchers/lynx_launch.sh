2e7430ffed6bf386b3aeaf525a403232c5ce5ea4
DrUm78 <cruau.david@gmail.com>
2025-03-21 18:41:33 +0100

    Libretro cores are now stored by default in the Linux partition for a few purposes: - prevent any unintentional deletion - users can still override the default cores by placing theirs into Libretro/cores/ (launcher scripts will search this location first) - provide an easy way to restore the default cores: just remove yours from Libretro/cores/ and that's it!

--- FunKey/board/funkey/rootfs-overlay/usr/games/launchers/lynx_launch.sh
+++ FunKey/board/funkey/rootfs-overlay/usr/games/launchers/lynx_launch.sh
@@ -9,7 +9,11 @@ fi
 
 # Launch the process in background, record the PID into a file, wait
 # for the process to terminate and erase the recorded PID
-picoarch /mnt/Libretro/cores/mednafen_lynx_libretro.so "$1"&
+if [ -e /mnt/Libretro/cores/mednafen_lynx_libretro.so ]; then
+	picoarch /mnt/Libretro/cores/mednafen_lynx_libretro.so "$1"&
+else
+	picoarch /usr/games/mednafen_lynx_libretro.so "$1"&
+fi
 pid record $!
 wait $!
 pid erase
