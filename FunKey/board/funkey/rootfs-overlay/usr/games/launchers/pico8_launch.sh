ca0cadc7dbb228f6b30ae57d86308769328c944a
DrUm78 <cruau.david@gmail.com>
2025-08-24 16:46:30 +0200

    [RetroFE][GMenu2X] Added new systems Final Burn Alpha 2012, MAME 2000, PICO-8 (FAKE-08) and Pokémon Mini (Pokemini) [RetroFE][GMenu2X] Alias files are preloaded by default for FBA 2012 and MAME 2000 [RetroFE] Updated assets for the themes "RetroRoomCovers", "Game Boy" and "Superlopez" to support the new systems [RetroFE] Added default blank files for each theme sections for more consistency with default themes [GMenu2X] Added an OPK in "Settings" section that automatically reinstalls freeware games Split previous "mnt_files" between homebrew games ("freeware_games.zip") and FunKey data ("funkey_files.zip") during OS update Added 10 homebrew games for PICO-8 and 12 for Pokémon Mini

---
+++ FunKey/board/funkey/rootfs-overlay/usr/games/launchers/pico8_launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Launch the process in background, record the PID into a file, wait
+# for the process to terminate and erase the recorded PID
+if [ -e /mnt/Libretro/cores/fake08_libretro.so ]; then
+	picoarch /mnt/Libretro/cores/fake08_libretro.so "$1"&
+else
+	picoarch /usr/games/fake08_libretro.so "$1"&
+fi
+pid record $!
+wait $!
+pid erase
